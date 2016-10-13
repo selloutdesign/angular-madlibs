5b176aebcb6375eb19cbeebc05ce6ceef7dbc2e6
selloutdesign <j.selloutdesign@gmail.com>
2016-10-13 10:37:02 -0700

    controller

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
-var app = angular.module('plunker', []);
+var app = angular.module('plunker', ['ngMaterial']);
 
 app.controller('MainCtrl', function($scope) {
+	$scope.data = {
+		
+	};
+  
+	
+	$scope.onChange = function(cbState) {
+		if (cbState === "Female") {
+				$scope.sex = "she";
+				$scope.sex2 = "her";
+			}else {
+				$scope.sex = "he";
+				$scope.sex2 = "his";
+			}
+	};
   $scope.name = 'World';
+  $scope.femaleName = "Nancy";
 });
